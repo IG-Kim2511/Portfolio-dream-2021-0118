fecb9112b310fb94ea9583a4abb5885722d978d0
iikim <iikim2511@gmail.com>
2021-01-18 23:23:23 -0500

    home

--- script.js
+++ script.js
@@ -1,19 +1,47 @@
 "use strict";
 
-//🦄c2 자바스크립트의 목적과 셀렉터
-document.write("● C 02 자바스크립트의 목적과 셀렉터");
-console.log("● C 02 자바스크립트의 목적과 셀렉터");
 
-//  C 58 . JS 58
 
-// [Selectors]
+//  C 58 . JS 58 
+// s how navbar  with scroll (make navbar transparne t when it is on the top)
 
-// [Event Listeners]
+//  height
+ /* 
+ google ; js scroll position scrollY
 
-// [Funcitons]
-function name(event) {
-  // todo DIV
-  // Create LI
+ scrolly window.scrollY
+ 1. window.scroll y navbarHeight 
+ 'navbar--dark  add
+ remove
+
+ navbar--dark
+
+ */
+
+ const navbar = document.querySelector('#navbar');
+ const navbarHeight = navbar.getBoundingClientRect().height;
+
+ document.addEventListener('scroll',()=>{
+   if (window.scrollY>navbarHeight) {
+     navbar.classList.add('navbar--dark');
+     
+   } else {
+    navbar.classList.remove('navbar--dark');
+   }
+ });
+
+
+//  js 72 remove selection from the previous item and select the next selection
+
+const workBtnContainer = document.querySelector('.work__categories');
+
+workBtnContainer.addEventListener('click',(e)=>{
+  
+const active = document.querySelector('.category__btn.selected');
+if(active != null){
+  active.classList.remove('selected');
+
 }
+e.target.classList.add('selected');
 
-
+});
