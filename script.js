c692eef66af579b7d13f4d6a66049dfb8d58f0db
iikim <iikim2511@gmail.com>
2021-01-19 23:26:56 -0500

    done

--- script.js
+++ script.js
@@ -31,17 +31,84 @@
  });
 
 
-//  js 72 remove selection from the previous item and select the next selection
+//  js 60 handle scrolling when tapping on the navbar menu. js scroll to id
+/* click event
 
-const workBtnContainer = document.querySelector('.work__categories');
+dataset*/
 
-workBtnContainer.addEventListener('click',(e)=>{
-  
-const active = document.querySelector('.category__btn.selected');
-if(active != null){
-  active.classList.remove('selected');
+const navbarMenu = document.querySelector('.navbar__menu');
+
+navbarMenu.addEventListener('click',(e)=>{
+console.log(e.target)
+console.log(e.target.dataset.link)
+
+const scrollTo = document.querySelector(link);
+scrollTo.scrollIntoView({behavior:'smooth'});
+
+
+if(link ==null){
+  return;
 
 }
-e.target.classList.add('selected');
+navbarMenu.classList.remove('open');
 
+});
+
+// js 60_2 contact me
+ /* scrollintoview'
+ menu
+ selector
+  */
+
+  const homeContactBtn = document.querySelector('.home__contact');
+
+  homeContactBtn.addEventListener('click',()=>{
+    scrollIntoView(#contact);
+  });
+
+
+
+ //js 90 show arrow up 
+
+ /* document.addEventListener('scroll',()=>{
+   const arrowUp = document.querySelector('.arrow-up');
+
+   if(scrollY>homeHeight/2){
+    arrowUp.classList.add('visible');
+  }else{
+     arrowUp.classList.remove('visible');
+
+   }
+ }); */
+
+
+//  js 72 remove selection from the previous item and select the next selection
+
+const workBtnContainer = document.querySelector(".work__categories");
+const projectContainer = document.querySelector(".work__projects");
+const projects = document.querySelectorAll(".project");
+
+workBtnContainer.addEventListener('click',(e)=>{
+  const filter = e.target.dataset.filter || e.target.parentsNode.dataset.filter;
+
+
+  if(filter == null){
+    return;
+
+  }
+  // 68-2 forEach
+
+  projects.forEach((project)=>{
+    console.log(project.dataset.type);
+    
+    if (filter ==="" || filter === project.dataset.type) {
+      project.classList.remove('invisible');
+      
+    } else {
+      project.classList.add('invisible');
+      
+    }
+  });
+
+  // js 70
 });
